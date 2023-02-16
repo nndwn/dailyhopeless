6f4edd6a072d2127e7081d9df91fbf0397735d04
nndwn <nandawan.libya@gmail.com>
2023-02-17 03:44:30 +0700

    update error

--- src/pages/{LangJson.name}.js
+++ src/pages/{LangJson.name}.js
@@ -1,10 +1,21 @@
 import React from "react";
-import Layout  from "gatsby-plugin-image";
+import { graphql } from 'gatsby'
+import Layout from "../components/layout/layout";
 
-const Lang = ({children}) => {
+const Lang = ({children, data}) => {
     return(
-        <Layout>
+        <Layout pagepage={data.langJson.name} >
             {children}
         </Layout>
     )
 }
+
+export const query = graphql`
+query($id: String) {
+  langJson(id: {eq: $id}) {
+    name
+  }
+}
+`
+
+export default Lang
