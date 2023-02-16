4828b1ad3081087ed0f0e2bf9e4e4a7242f78a1a
nndwn <nandawan.libya@gmail.com>
2023-02-17 03:29:06 +0700

    update error

---
+++ src/pages/{LangJson.name}.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout  from "gatsby-plugin-image";
+
+const Lang = ({children}) => {
+    return(
+        <Layout>
+            {children}
+        </Layout>
+    )
+}
